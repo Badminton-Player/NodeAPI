3da5bc2c6bb73b72dbd6849ca7c404ab6d4aa60f
Ankush kumar <ankush.targhotra@gmail.com>
2021-10-20 09:37:01 +0530

    Complete the task

---
+++ app.js
@@ -0,0 +1,174 @@
+require('dotenv').config()
+const express = require('express')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+const passport = require('passport')
+const session = require('express-session')
+const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const findOrCreate = require('mongoose-findorcreate')
+
+
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(session({
+    secret:"My little secret",
+    resave:false,
+    saveUninitialized:false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect('mongodb://localhost:27017/NodeAPIDB', {useNewUrlParser: true})
+
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String,
+    googleId:String,
+    task:String
+})
+
+
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
+
+const User =new mongoose.model('User', userSchema)
+
+passport.use(User.createStrategy())
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  })
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    })
+  })
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/node",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    })
+  }
+))
+
+
+app.get('/', function(req,res){
+    res.render('home')
+})
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }))
+
+
+  app.get('/auth/google/node', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+  
+    res.redirect('/task');
+  })
+
+
+
+app.get('/login',function(req,res){
+    res.render('login')
+})
+
+app.get('/register',function(req,res){
+    res.render('register')
+})
+
+app.get('/logout', function(req, res){
+    req.logout()
+    res.redirect('/')
+  });
+
+app.get('/task', function(req,res){
+   User.find({'task': {$ne:null}}, function(err, foundUsers){
+       if(err){
+           console.log(err)
+       } else {
+           if(foundUsers){
+           res.render('task', {userWithTasks:foundUsers})
+           }
+       }
+   })
+})
+
+app.get('/submit', function(req,res){
+    if(req.isAuthenticated()){
+        res.render('submit')
+    }else{
+        res.redirect('/login')
+    }
+})
+
+app.post('/submit', function(req,res){
+
+    const submitTask = req.body.task
+    
+    User.findById(req.user.id, function(req, foundUser){
+        if(err){
+            console.log(err)
+        }  else {
+            if(foundUser){
+                foundUser.task = submitTask
+                foundUser.save(function(){
+                    res.redirect('/task')
+                })
+            }
+        }
+    })
+})
+
+app.post('/register', function(req,res){
+ User.register({username:req.body.username}, req.body.password, function(err, user){
+     if(err){
+         console.log(err)
+         res.redirect('/register')
+     }else{
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/task')
+        })
+     }
+ })
+  
+})
+
+app.post('/login', function(req,res){
+  
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
+    })
+    req.login(user, function(err){
+        if(err){
+            console.log(err)
+        } else{
+            passport.authenticate('local')(req, res, function(){
+                res.redirect('/task')
+            })
+        }
+    })
+})
+
+
+app.listen(3000, function(){
+    console.log("server has started on http://localhost:3000");
+})
